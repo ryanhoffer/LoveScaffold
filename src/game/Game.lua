81d7ad62b4951d9b61683f2d1f3dce49bcca5b97
Ryan Hoffer <ryanlhoffer@gmail.com>
2026-01-31 17:04:45 -0500

    fixed gamescene references

--- src/game/Game.lua
+++ src/game/Game.lua
@@ -22,25 +22,6 @@ end
 
 
 function Game:draw()
-	-- Draw an infinite dotted grid based on camera and screen size
-	love.graphics.setColor(0.5, 1, 0, 0.5) -- lime green
-	local gridSize = 64
-	local camX = self.player.x - love.graphics.getWidth() / 2
-	local camY = self.player.y - love.graphics.getHeight() / 2
-	local left = math.floor((camX - love.graphics.getWidth()) / gridSize) * gridSize
-	local right = math.ceil((camX + love.graphics.getWidth() * 2) / gridSize) * gridSize
-	local top = math.floor((camY - love.graphics.getHeight()) / gridSize) * gridSize
-	local bottom = math.ceil((camY + love.graphics.getHeight() * 2) / gridSize) * gridSize
-
-	local plusSize = 4
-	for x = left, right, gridSize do
-		for y = top, bottom, gridSize do
-			-- Draw a plus sign: vertical and horizontal lines crossing at (x, y)
-			love.graphics.line(x - plusSize, y, x + plusSize, y)
-			love.graphics.line(x, y - plusSize, x, y + plusSize)
-		end
-	end
-	love.graphics.setColor(1, 1, 1)
 	for _, entity in ipairs(self.entities) do
 		entity:draw()
 	end
--- src/scenes/GameScene.lua
+++ src/scenes/GameScene.lua
@@ -30,6 +30,25 @@ end
 
 function GameScene:draw()
     self.camera:attach()
+    -- Draw an infinite dotted grid based on camera and screen size
+    love.graphics.setColor(0.5, 1, 0, 0.5) -- lime green
+    local gridSize = 64
+    local camX = self.game.player.x - love.graphics.getWidth() / 2
+    local camY = self.game.player.y - love.graphics.getHeight() / 2
+    local left = math.floor((camX - love.graphics.getWidth()) / gridSize) * gridSize
+    local right = math.ceil((camX + love.graphics.getWidth() * 2) / gridSize) * gridSize
+    local top = math.floor((camY - love.graphics.getHeight()) / gridSize) * gridSize
+    local bottom = math.ceil((camY + love.graphics.getHeight() * 2) / gridSize) * gridSize
+
+    local plusSize = 4
+    for x = left, right, gridSize do
+        for y = top, bottom, gridSize do
+            -- Draw a plus sign: vertical and horizontal lines crossing at (x, y)
+            love.graphics.line(x - plusSize, y, x + plusSize, y)
+            love.graphics.line(x, y - plusSize, x, y + plusSize)
+        end
+    end
+    love.graphics.setColor(1, 1, 1)
     self.game:draw()
     self.camera:detach()
 end
